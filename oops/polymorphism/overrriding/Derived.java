f808482504cdb8c0c54cac0b6ed3280ad3c21adb
Apeksha-Shah <apekshashah029@gmail.com>
2025-12-08 16:44:30 +0530

    modified

--- oops/polymorphism/overrriding/Derived.java
+++ oops/polymorphism/overrriding/Derived.java
@@ -6,18 +6,28 @@ class base{
     void print(){
         System.out.println("from base");
     }
+
+    private void show(){
+        System.out.println("show from base");
+    }
 }
 public class Derived extends base {
 
     void print(){
         System.out.println("from derived");
     }
+
+    void show(){
+        System.out.println("show from derived");
+    }
     public static void main(String[] args) {
         Derived d = new Derived();
         d.print();
+        d.show();
 
         base b = new Derived();     // upcasting
         b.print();
+        // b.show(); // this won't work bcz show is not overridden
 
         base b1 = new base();
         b1.print();
