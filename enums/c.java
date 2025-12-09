826ea7cfcd945e1648b919bacdd55fc8cb16f076
Apeksha-Shah <apekshashah029@gmail.com>
2025-12-09 11:13:48 +0530

    Enum and optional done

---
+++ enums/c.java
@@ -0,0 +1,55 @@
+package enums;
+
+import java.util.Optional;
+
+enum Status{
+    STARTED{
+        public void print(){
+            System.out.println(this + " phase");
+        }
+
+    }, PENDING{
+        public void print(){
+            System.out.println(this + " phase");
+        }
+    }, PAUSED{
+        public void print(){
+            System.out.println(this + " phase");
+        }
+
+    }, STOPPED{
+        public void print(){
+            System.out.println(this + " phase");
+        }
+
+    };
+
+    private Status(){
+        System.out.println("Constructor called for: "+ this);
+    }
+
+    public void display(){
+        System.out.println("Status is : "+this);
+    }
+
+
+    // if any abstract method is present in enum then constants must have to implement that method
+    public abstract void print();
+}
+public class c {
+    public static void main(String[] args) {
+        // can't create enum object using new object for constants already created when class loaded
+        // for the first time, fixed object
+
+        Status s = Status.STARTED;
+        s.display();
+        s.print();
+
+        Optional<String> name = Optional.of("Apeksha");
+        System.out.println(name.orElse("No name"));
+
+        Optional<String> name1 = Optional.ofNullable(null);
+        System.out.println(name1.orElse("No name"));
+
+    }
+}
